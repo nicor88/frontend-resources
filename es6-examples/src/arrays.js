7f8544db8e921d47049537665bc9b7d0ac7a1972
nicor88 <ncorda@babbel.com>
2016-12-23 15:19:14 +0100

    added linting

--- es6-examples/src/arrays.js
+++ es6-examples/src/arrays.js
@@ -1,9 +1,9 @@
 // Expression bodies
-var odds = evens.map(v => v + 1);
-var nums = evens.map((v, i) => v + i);
+const array = [1, 2, 3, 4];
+const odds = array.map((v) => v + 1);
+const nums = array.map((v, i) => v + i);
 
-// Statement bodies
-nums.forEach(v => {
-  if (v % 5 === 0)
-fives.push(v);
-});
+// Sets examples
+const s = new Set();
+s.add('hello').add('goodbye').add('hello');
+// s.size === 2 -> True
